f6b15972af6309d850f56a47e28e68459302dfb0
ollreee <49379969+ollreee@users.noreply.github.com>
2019-04-08 04:32:21 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+#This file is used to tweak game settings.
+#Most likely, you won't want to touch anything here.
+
 #Colors used for rendering certain graphics
 GREEN = (0, 255, 0)
 RED = (179, 0, 0)
